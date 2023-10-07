83164ee7d23185322c2c81882174b3c96ef13bd7
AksanaChopchyts <aksana.chopchyts@gmail.com>
2023-10-07 18:30:13 +0300

    Task #2 has been  added.

--- Order.cs
+++ Order.cs
@@ -2,10 +2,58 @@
 {
     internal class Order
     {
-        public string ProductName;
-        public long PhoneNumber;
-        public float Cost;
-        public string DeliveryAddress;
+        private string _productName;
+        public string ProductName
+        {
+            get
+            {
+                return _productName;
+            }
+            set
+            {
+                _productName = value;
+            }
+        } 
+        private long _phoneNumber;
+        public long PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                if (value.ToString().Length == 13)
+                    _phoneNumber = value;
+            }
+        }
+
+        private float _cost;
+        public float Cost
+        {
+            get
+            {
+                return _cost;
+            }
+            set
+            {
+                if (value > 0 && value <= 1000)
+                    _cost = value;
+            }
+        }
+        private string _deliveryAddress;
+        public string DeliveryAddress
+        {
+            get
+            {
+                return _deliveryAddress;
+            }
+            set
+            {
+                _deliveryAddress = value;
+            }
+        }
+
         public Order(string productName, long phoneNumber, float cost, string deliveryAddress)
         {
             ProductName = productName;
--- Program.cs
+++ Program.cs
@@ -4,10 +4,33 @@ using AksanaChopchytsAutoQATasks;
 
 Order[] orders = {new Order ("Ice-cream", 3752961111116, 3f, "Timiryazev St"),
                   new Order ( "Milk", 3752951111115, 5f, "Orlovskay St"),
-                  new Order ("Whyskey", 3721111111112, 25f,"Sedova St") };
+                  new Order ("Whyskey", 3721111111112, 25f,"Sedova St"),
+                  new Order ("Whysk", 3721111111112, 15f,"Sedova St")};
+
+
+Console.WriteLine(" - - - Task #2 All items - - - ");
 
 foreach (Order oder in orders)
 {
     Console.WriteLine(oder.GetFullInfo());
 }
-                                       
+
+Console.WriteLine(" - - - Task #2.4 - - - ");
+
+foreach (Order order in orders)
+{
+    if (order.PhoneNumber.ToString().StartsWith("375"))
+    {
+        Console.WriteLine(order.GetFullInfo());
+    }
+}
+
+Console.WriteLine(" - - - Task #2.5 - - - ");
+
+foreach (Order order in orders)
+{
+    if (order.Cost < 20 && order.ProductName.ToString().StartsWith("Whys"))
+    {
+        Console.WriteLine(order.GetFullInfo());
+    }
+}
