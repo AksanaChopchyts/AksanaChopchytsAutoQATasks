345618b30a6d8a2e46e4dabe6899ad4d5778894e
USER <USER@AKSANA-PROBOOK>
2023-10-07 17:31:19 +0300

    Add project files.

---
+++ Order.cs
@@ -0,0 +1,22 @@
+﻿namespace AksanaChopchytsAutoQATasks
+{
+    internal class Order
+    {
+        public string ProductName;
+        public long PhoneNumber;
+        public float Cost;
+        public string DeliveryAddress;
+        public Order(string productName, long phoneNumber, float cost, string deliveryAddress)
+        {
+            ProductName = productName;
+            PhoneNumber = phoneNumber;
+            Cost = cost;
+            DeliveryAddress = deliveryAddress;
+        }
+        public string GetFullInfo()
+        {
+            return $"Product Name:{ProductName}, PhoneNumber:{PhoneNumber}, Cost:{Cost}, DeliveryAddress: {DeliveryAddress}";
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AksanaChopchytsAutoQATasks;
+
+
+Order[] orders = {new Order ("Ice-cream", 3752961111116, 3f, "Timiryazev St"),
+                  new Order ( "Milk", 3752951111115, 5f, "Orlovskay St"),
+                  new Order ("Whyskey", 3721111111112, 25f,"Sedova St") };
+
+foreach (Order oder in orders)
+{
+    Console.WriteLine(oder.GetFullInfo());
+}
+                                       
