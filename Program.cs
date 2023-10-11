35af96719ce23af83caf605303db54e27dd1f7da
AksanaChopchyts <aksana.chopchyts@gmail.com>
2023-10-11 19:52:00 +0300

    Task #5.4-5.6 has been added.(5.6 works if we have only 1!!! the most common product)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AksanaChopchytsAutoQATasks;
 using AksanaChopchytsAutoQATasks.Comparers;
-
+using System.Collections.Generic;
+using System.Linq;
 
 List<Order> orders = new List<Order>();
 orders.Add(new OrdinaryOrder("Ice-cream", 3752961111116, 3f, "Timiryazev St"));
@@ -12,29 +13,25 @@ orders.Add(new OrdinaryOrder("Whysk", 3721111111112, 15f, "Sedova St"));
 orders.Add(new DiscountOrder("Mouse", 3751234567892, 25f, "Orlovskay St", -10f));
 orders.Add(new VIPOrder("Laptop", 3751234567890, 900f, "Timiryazev St", "Cover"));
 
-var orderByNameComparer = new OrderByNameComparer();
-orders.Sort(orderByNameComparer);
+var sortedByName = orders.OrderBy(order => order.ProductName);
+var sortedByCost = orders.OrderBy(order => order.Cost);
+var sortedByAddress = orders.OrderBy(order => order.DeliveryAddress);
 
-Console.WriteLine("Sorted by Name:");
-foreach (var order in orders)
+var affordable_Products = orders
+    .Where(order => order.Cost <= 100)
+    .OrderBy(order => order.ProductName)
+    .Select(productName => productName);
+
+foreach (var productName in affordable_Products)
 {
-    Console.WriteLine(order.GetFullInfo());
+    Console.WriteLine(productName);
 }
 
-var orderByCostComparer = new OrderByCostComparer();
-orders.Sort(orderByCostComparer);
+// Works if we have only 1!!! the most common product on the order list
+var mostFrequentProduct = orders
+    .GroupBy(orders => orders.ProductName)
+    .OrderByDescending(group => group.Count())
+    .Select(group => group.Key)
+    .First();
 
-Console.WriteLine("Sorted by Cost:");
-foreach (var order in orders)
-{
-    Console.WriteLine(order.GetFullInfo());
-}
-
-var orderByAddressComparer = new OrderByDeliveryAddressComparer();
-orders.Sort(orderByAddressComparer);
-
-Console.WriteLine("Sorted by Address:");
-foreach (var order in orders)
-{
-    Console.WriteLine(order.GetFullInfo());
-}
+Console.WriteLine("Most frequent product: " + mostFrequentProduct); 
